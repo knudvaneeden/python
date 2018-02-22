a309f314ccc7e5d5271b161deebdfb9f23dcd6f0
knudvaneeden <knud_van_eeden@yahoo.com>
2018-02-23 00:11:36 +0100

    Last commit: 23/02/2018  0:11:35

--- ddd.py
+++ ddd.py
@@ -7,10 +7,14 @@ import numpy
 # fix random seed for reproducibility
 numpy.random.seed(7)
 # load pima indians dataset
-dataset = numpy.loadtxt("ddd00.csv", delimiter=",")
+dataset = numpy.loadtxt("dddtrain.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X_train = dataset[:,0:7036]
 y_train = dataset[:,7036]
+dataset = numpy.loadtxt("dddtest.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X_test = dataset[:,0:7036]
+y_test = dataset[:,7036]
 # create model
 model = Sequential()
 model.add(Dense(12, input_dim=7036, activation='relu'))
@@ -24,10 +28,10 @@ model.fit(X_train, y_train, epochs=150, batch_size=10)
 scores = model.evaluate(X_train, y_train)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 # calculate predictions
-predictions = model.predict(X_train)
+predictions = model.predict( X_test )
 # round predictions
 rounded = [ x[0] for x in predictions]
 rounded = numpy.multiply( rounded, 100 )
 rounded = numpy.round( rounded, decimals=0, out=None )
 numpy.set_printoptions( threshold=numpy.inf )
-print(rounded)
+print( rounded )
